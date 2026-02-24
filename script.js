f587b2cdf4d9753211b98f14531e27008f38fdee
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-02-24 17:47:10 +0600

    start js

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ let interview = document.getElementById("interview");
 let rejected = document.getElementById("rejected");
 let jobAvailable = document.getElementById("job-available");
 
+const allBtn = document.getElementById("all-btn");
+const interviewBtn = document.getElementById("interview-btn");
+const rejectedBtn = document.getElementById("rejected-btn");
+
 const allCardSection = document.getElementById("all-card");
 
-console.log(allCardSection.children.length);
+// console.log(allCardSection.children.length);
+
+const allCard = document.getElementById("all-card")
+const mainContainer = document.querySelector('main')
 
 function totalJobCount(){
     total.innerText = allCardSection.children.length;
@@ -16,3 +23,14 @@ function totalJobCount(){
 }
 
 totalJobCount();
+
+function toggleStyle(id){
+    console.log(id)
+}
+
+mainContainer.addEventListener('click',
+    function(event){
+
+        console.log(event.target.parentNode.parentNode)
+    }
+)
